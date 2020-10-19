5caf3d1fc8edd3b79a1e7f62de29a42140989087
AlexBirle1412 <alex_birle@yahoo.com>
2020-10-19 09:54:51 +0300

    a little debugging

--- index.php
+++ index.php
@@ -1,93 +1,105 @@
-<!DOCTYPE HTML>  
+<!DOCTYPE HTML>
 <html>
+
 <head>
-<style>
-.error {color: #FF0000;}
-</style>
+    <style>
+        .error {
+            color: #FF0000;
+        }
+    </style>
 </head>
-<body>  
 
-<?php
-// Create connection
-$conn = new mysqli('remotemysql.com', 'fHwtL0NTeB', 'Cc4vBFGO8H');
+<body>
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-// define variables and set to empty values
-$nameErr = $emailErr = $passwordErr = "";
-$name = $email = $password = "";
+    <?php
+    // Create connection
+    $conn = new mysqli('remotemysql.com', 'TGOhJ1gk0h', 'KLAwS8PITW');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
+    echo "Connected successfully";
+    // define variables and set to empty values
+    $nameErr = $emailErr = $passwordErr = "";
+    $name = $email = $password = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST["name"])) {
+            $nameErr = "Name is required";
+        } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                $nameErr = "Only letters and white space allowed";
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email is required";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+        $password = test_input($_POST["password"]);
+
+        $sql = "INSERT INTO TGOhJ1gk0h.`collectted_data` (name, email, password)
+                       VALUES ('$name', '$email', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $sql = "SELECT * FROM fHwtL0NTeB.form_data";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while ($row = $result->fetch_assoc()) {
+                echo "name: " . $row["name"] . " - email: " . $row["email"] . " - password: " . $row["password"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
     }
-  }
-  $password = test_input($_POST["password"]);
-  $sql = "INSERT INTO fHwtL0NTeB.form_data (name, email, password)
-    VALUES ('$name', '$email', '$password')";
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$sql = "SELECT * FROM fHwtL0NTeB.form_data";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "name: " . $row["name"]. " - email: " . $row["email"]. " - password: " . $row["password"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-}
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
 
-<h2>PHP Form Validation Example</h2>
-<p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br>
-  E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br>
-  Password: <input type="password" name="password" value="<?php echo $password;?>">
-  <br><br>>
-  <input type="submit" name="submit" value="Submit">  
-</form>
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $password;
-?>
+    ?>
+
+    <h2>PHP Form Validation Example</h2>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        Name: <input type="text" name="name" value="<?php echo $name; ?>">
+        <span class="error">* <?php echo $nameErr; ?></span>
+        <br><br>
+        E-mail: <input type="text" name="email" value="<?php echo $email; ?>">
+        <span class="error">* <?php echo $emailErr; ?></span>
+        <br><br>
+        Password: <input type="password" name="password" value="<?php echo $password; ?>">
+        <br><br>>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $password;
+    ?>
 </body>
+
 </html>
